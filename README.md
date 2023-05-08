7f6c99f2307b453bf5624a6497efd7b3bbe27b33
ifourrahulp <105262282+ifourrahulp@users.noreply.github.com>
2023-05-08 11:02:10 +0530

    setup firebase

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # firebaseAuthProject
 
+https://medium.com/firebase-developers/firebase-authentication-f0445ac732cb
+
+https://stackblitz.com/edit/firebase-authentication-with-google-wgjpmr?file=readme.md,src%2Fapp%2Fapp.module.ts
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-r5yats)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,53 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 import { AppComponent } from './app.component';
+
+var config = {
+  apiKey: 'AIzaSyAAAjTntCh9aLSmKolqFJLep3d6avf9aYI',
+  authDomain: 'mydb2018-fcd4d.firebaseapp.com',
+  databaseURL: 'https://mydb2018-fcd4d.firebaseio.com',
+  projectId: 'mydb2018-fcd4d',
+  storageBucket: 'mydb2018-fcd4d.appspot.com',
+  messagingSenderId: '1002281455405',
+};
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    {
+      scopes: ['public_profile', 'email', 'user_likes', 'user_friends'],
+      customParameters: {
+        auth_type: 'reauthenticate',
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    },
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    },
+  ],
+  //term of service
+  tosUrl: '<your-tos-link>',
+  //privacy url
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  //credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+};
+
 @NgModule({
   declarations: [],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
+  ],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],
